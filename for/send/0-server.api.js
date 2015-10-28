e5f802f6cd637ce0e0b4cb2db1295d7d8812c746
Christoph Dorn <christoph@christophdorn.com>
2015-10-27 21:17:40 -0700

    better 404 handling

--- for/send/0-server.api.js
+++ for/send/0-server.api.js
@@ -6,16 +6,27 @@ exports.app = function (options) {
 
     return function (req, res, next) {
         var uri = req.params[0];
-        var paths = Object.keys(options.paths).map(function (alias) {
+        var allPaths = Object.keys(options.paths).map(function (alias) {
             return options.paths[alias]
         });
+        var paths = [].concat(allPaths);
         function tryNextPath () {
             var basePath = paths.shift();
             return SEND(req, uri, {
         		root: basePath
         	}).on("error", function (err) {
                 if (paths.length === 0) {
-                    return next(err);
+                    // We silence the error and continue.
+            	    console.log("File '" + uri + "' not found in basePaths '" + allPaths.join("', '") + "'", new Error().stack);
+            	    if (!req.flags) {
+            	        req.flags = {};
+            	    }
+            	    // We tell the page resolver to 404 when no exact match found
+            	    // TODO: Make this more generic.
+            	    req.flags["export.sm.hoist.VisualComponents"] = {
+            	        "404onRootPage": true
+            	    };
+                    return next();
                 }
         	    return tryNextPath();
         	}).pipe(res);
