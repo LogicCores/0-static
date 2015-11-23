d29a59c1cf62137e97bd072552a1d1348408aa57
Christoph Dorn <christoph@christophdorn.com>
2015-11-23 13:34:34 -0800

    static assets per rev

--- for/send/0-server.api.js
+++ for/send/0-server.api.js
@@ -24,6 +24,21 @@ exports.forLib = function (LIB) {
             });
             var paths = [].concat(allPaths);
             function tryNextPath () {
+
+                if (paths.length === 0) {
+                    // We silence the error and continue.
+            	    console.log("File '" + uri + "' not found in basePaths '" + allPaths.join("', '") + "'", new Error().stack);
+            	    if (!req.flags) {
+            	        req.flags = {};
+            	    }
+            	    // We tell the page resolver to 404 when no exact match found
+            	    // TODO: Make this more generic.
+            	    req.flags["export.sm.hoist.VisualComponents"] = {
+            	        "404onRootPage": true
+            	    };
+                    return next();
+                }
+
                 var basePath = paths.shift();
 
 
@@ -31,72 +46,89 @@ exports.forLib = function (LIB) {
                 // TODO: Optionally make all URIs relative.
                 // TODO: Put this into a transformer that gets declared in config.
                 if (
-                    options.subUri &&
+//                    options.subUri &&
                     /\.css$/.test(uri)
                 ) {
 
-                    var distPath = LIB.path.join(options.cachePath, uri);
+                    var sourcePath = LIB.path.join(basePath, uri);
 
-                    function serveDistPath () {
-                        return LIB.send(req, LIB.path.basename(distPath), {
-                    		root: LIB.path.dirname(distPath),
-                    		maxAge: options.clientCacheTTL || 0
-                    	}).on("error", next).pipe(res);
-                    }
+                    return LIB.fs.existsAsync(sourcePath).then(function (exists) {
+                        if (!exists) {
+                            return tryNextPath();
+                        }
 
-                    return LIB.fs.existsAsync(distPath).then(function (exists) {
-
-                        if (
-                            exists &&
-                            options.alwaysRebuild === false
-                        ) {
-                            if (LIB.VERBOSE) console.log("Using cached processed file for uri '" + uri + "' from cache '" + distPath + "'");
+                        var distPath = LIB.path.join(options.cachePath, uri);
     
-                            return serveDistPath();
+                        function serveDistPath () {
+                            return LIB.send(req, LIB.path.basename(distPath), {
+                        		root: LIB.path.dirname(distPath),
+                        		maxAge: options.clientCacheTTL || 0
+                        	}).on("error", next).pipe(res);
                         }
     
-                        const REWORK = require("rework");
-                        const REWORK_PLUGIN_URL = require("rework-plugin-url");
+                        return LIB.fs.existsAsync(distPath).then(function (exists) {
     
-                        var sourcePath = LIB.path.join(basePath, uri);
-                        
-                        return LIB.fs.readFileAsync(sourcePath, "utf8").then(function (data) {
+                            if (
+                                exists &&
+                                options.alwaysRebuild === false
+                            ) {
+                                if (LIB.VERBOSE) console.log("Using cached processed file for uri '" + uri + "' from cache '" + distPath + "'");
+        
+                                return serveDistPath();
+                            }
+        
+                            const REWORK = require("rework");
+                            const REWORK_PLUGIN_URL = require("rework-plugin-url");
+    
+                            var relativeBasePath = LIB.path.dirname(uri);
+    
+                            return LIB.fs.readFileAsync(sourcePath, "utf8").then(function (data) {
+    
+                				var output = REWORK(data, {
+                					source: sourcePath
+                				})
+                				.use(REWORK_PLUGIN_URL(function (url) {
 
-            				var output = REWORK(data, {
-            					source: sourcePath
-            				})
-            				.use(REWORK_PLUGIN_URL(function (url) {
-                                // Rewrite all absolute urls by prepending the subUri.
-            					if (/^\//.test(url)) {
-            					    url = options.subUri + url;
+                				    if (
+                				        options.stripCssPrefix &&
+                				        new RegExp("^" + LIB.RegExp_Escape(options.stripCssPrefix + "/" + relativeBasePath), "g").exec(url)
+                				    ) {
+                				        url = url.replace(new RegExp("^" + LIB.RegExp_Escape(options.stripCssPrefix + "/" + relativeBasePath), "g"), ".");
+                				    }
+    /*
+                                    // Rewrite all absolute urls by prepending the subUri.
+                					if (/^\//.test(url)) {
+                					    url = options.subUri + url;
+                					}
+    */
+                					return url;
+                				}));
+    
+                                var css = output.toString();
+        
+            					function checkIfChanged () {
+            						return LIB.fs.existsAsync(distPath).then(function (exists) {
+            							if (!exists) return true;
+            							return LIB.fs.readFileAsync(distPath, "utf8").then(function (existingData) {
+            								if (existingData === css) {
+            									return false;
+            								}
+            								return true;
+            							});
+            						});
             					}
-            					return url;
-            				}));
-
-                            var css = output.toString();
+            					
+            					return checkIfChanged().then(function (changed) {
+            						if (!changed) return null;
     
-        					function checkIfChanged () {
-        						return LIB.fs.existsAsync(distPath).then(function (exists) {
-        							if (!exists) return true;
-        							return LIB.fs.readFileAsync(distPath, "utf8").then(function (existingData) {
-        								if (existingData === css) {
-        									return false;
-        								}
-        								return true;
-        							});
-        						});
-        					}
-        					
-        					return checkIfChanged().then(function (changed) {
-        						if (!changed) return null;
-
-                                if (LIB.VERBOSE) console.log("Writing processed file for uri '" + uri + "' to cache '" + distPath + "'");
-    
-        				        return LIB.fs.outputFileAsync(distPath, css, "utf8");
-        					}).then(function () {
-        					    
-        					    return serveDistPath();
-        					});
+                                    if (LIB.VERBOSE) console.log("Writing processed file for uri '" + uri + "' to cache '" + distPath + "'");
+        
+            				        return LIB.fs.outputFileAsync(distPath, css, "utf8");
+            					}).then(function () {
+            					    
+            					    return serveDistPath();
+            					});
+                            });
                         });
                     }).catch(next);
                 }
@@ -106,19 +138,6 @@ exports.forLib = function (LIB) {
             		root: basePath,
             		maxAge: options.clientCacheTTL
             	}).on("error", function (err) {
-                    if (paths.length === 0) {
-                        // We silence the error and continue.
-                	    console.log("File '" + uri + "' not found in basePaths '" + allPaths.join("', '") + "'", new Error().stack);
-                	    if (!req.flags) {
-                	        req.flags = {};
-                	    }
-                	    // We tell the page resolver to 404 when no exact match found
-                	    // TODO: Make this more generic.
-                	    req.flags["export.sm.hoist.VisualComponents"] = {
-                	        "404onRootPage": true
-                	    };
-                        return next();
-                    }
             	    return tryNextPath();
             	}).pipe(res);
             }
